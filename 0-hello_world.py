a95b29a7ba90e7c9b3d0fb40916ba2fab1089394
unknown <Filipi231>
2023-10-27 18:55:48 -0300

    mais aulas

---
+++ 0-hello_world.py
@@ -0,0 +1,3 @@
+print("Hello World!")
+print("Opa funcionou")
+print("Opa funcionou")
---
+++ aula3.py
@@ -0,0 +1,22 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Filipi Luiz')
+print(1, 'Filipi "Luiz"')
+
+# Aspas duplas
+print("Filipi Luiz")
+print(2, "Filipi 'Luiz''")
+
+# Escape
+print("Filipi \"Luiz\"")
+
+# r
+print(r"Filipi \"Luiz\"")
