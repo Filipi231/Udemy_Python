96ce3b593f3517e21da141b79f2d39bdb640aa58
Filipi <filipibarbosa750@outlook.com>
2023-11-03 13:25:44 -0300

    novas aulas

---
+++ aula8.py
@@ -0,0 +1,15 @@
+nome = "Filipi";
+sobrenome = "Luiz";
+idade = 23;
+ano_nascimento = 2023 - idade;
+maior_de_idade = idade >= 18;
+altura_metros = 1.76;
+
+
+print("Nome", nome);
+print("Sobrenome", sobrenome);
+print("Idade", idade);
+print("Ano de Nascimento", ano_nascimento);
+print("É maior de idade?", maior_de_idade);
+print("Altura em Metros", altura_metros);
+
